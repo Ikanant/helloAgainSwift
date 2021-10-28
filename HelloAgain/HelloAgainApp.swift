870ee556bce90a44c424b5fc09db97b09f9f9a00
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2021-10-28 09:48:15 -0400

    Initial Commit

---
+++ HelloAgain/HelloAgainApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloAgainApp.swift
+//  HelloAgain
+//
+//  Created by Jonathan Hernandez on 10/28/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloAgainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
